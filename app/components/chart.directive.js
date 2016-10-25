9ba42890e30390a0eec9b2a65753edaaf19aace0
Stefan Bechert <bechert@infai.org>
2016-10-25 15:19:57 +0200

    [refactor] Neue Zeitreihen Struktur. Fehler noch nicht alle behoben.

--- app/components/chart.directive.js
+++ app/components/chart.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp')
-    .directive('chart', function (DataConverter, DefaultTimeseriesDefinition, DividingService, AggregatingService, Timeseries) {
+    .directive('chart', function (DataConverter, DefaultTimeseriesDefinition, DividingService, AggregatingService, TimeseriesUtil) {
 
         return {
             templateUrl: 'app/components/chart.html',
