9b507beda82db9b3b0e0d3807b87450b8dd67cae
Stefan Bechert <bechert@infai.org>
2016-10-28 12:07:49 +0200

    [release] 0.1.0

--- app/testcomponents/TestController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('TimeseriesAnalysationTool').controller('TestController', ['$scope', 'DefaultTimeseriesDefinition', function($scope, DefaultTimeseriesDefinition) {
-
-    DefaultTimeseriesDefinition.getDefaultArrayBasedTimeseries().then(function(res) {
-        $scope.data = res;
-    });
-
-}]);
